c0f08bef2823f9a1736c324e99f04e4a44667c21
hawkjosh <hawkjosh@icloud.com>
2023-02-16 16:47:15 -0500

    content update push

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,19 +4,18 @@ import { Outlet, useLocation } from 'react-router-dom'
 import '../assets/styles/Layout.css'
 
 import NavbarStatic from './NavbarStatic.jsx'
-// import NavbarHide from './NavbarHide.jsx'
 // import NavbarShrink from './NavbarShrink.jsx'
+// import NavbarHide from './NavbarHide.jsx'
 import Footer from './Footer.jsx'
 
 export default function Layout() {
-	const location = useLocation('/work-samples')
+	// const location = useLocation('/work-samples')
 	return (
 		<Fragment>
 			<section>
-
 				<NavbarStatic />
-				{/* <NavbarHide /> */}
 				{/* <NavbarShrink /> */}
+				{/* <NavbarHide /> */}
 				<Outlet />
 				<Footer />
 			</section>
